79f67d5414e5193ccd23c62c727832d98d648016
laszloekovacs <laszloekovacs@gmail.com>
2022-10-07 11:36:15 +0200

    removed unused vars

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import CounterContext, {useCounterContext} from './CounterContext';
 import User from './User';
 
 function App(): ReactElement {
-	const [state, dispatch] = useCounterContext();
-
 	return (
 		<>
 			<CounterContext>
